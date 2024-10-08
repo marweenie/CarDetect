e4c63ab089bb02294be65843fcaa7ae4929cef83
marweenie <133703779+marweenie@users.noreply.github.com>
2024-10-08 18:53:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/javaapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.javaapp;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d("MainActivity: ", "Opencv is loaded");
+        } else {
+            Log.d("MainActivity: ", "Opencv failed to load");
+        }
+    }
+
+
+    private Button camera_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        camera_button = findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
+
+}
